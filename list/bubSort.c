543810e50a60a7a9b69d0213ba9ae8e8435363f8
lnb <lnbsa12345@gmail.com>
2025-02-28 00:00:21 +0530

    an array representation for the alg

--- list/bubSort.c
+++ list/bubSort.c
@@ -37,3 +37,12 @@ node *bubSort(node *head) {
   }
   return head;
 }
+
+/* for (int i = 0; i < n - 1; i++) {
+ *   for (int j = i + 1; j < n; j++) {
+ *     if (arr[i] > arr[j]) {
+ *       swap(arr + i, arr + j);
+ *     }
+ *   }
+ * }
+ */
