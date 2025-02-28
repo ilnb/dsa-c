7cf8a953edc1a1906ba673afdd9a269dc24f1470
lnb <lnbsa12345@gmail.com>
2025-03-01 01:01:42 +0530

    circular list

---
+++ list/ring.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+  int key;
+  struct node *next;
+} node;
+
+void menu();
+node *insert(node *, int);
+node *delete(node *, int);
+void printRing(node *);
+node *freeRing(node *);
+
+int main() {
+  node *ring = 0;
+  int option;
+  do {
+    menu();
+    printf("What's on your mind? ");
+    scanf("%d", &option);
+    switch (option) {
+      case 1: {
+        printf("Enter the value you want to insert: ");
+        int val;
+        scanf("%d", &val);
+        ring = insert(ring, val);
+        break;
+      }
+      case 2: {
+        printf("Enter the value you want to delete: ");
+        int val;
+        scanf("%d", &val);
+        ring = delete (ring, val);
+        break;
+      }
+      case 3: {
+        printRing(ring);
+        break;
+      }
+      case 4: {
+        ring = freeRing(ring);
+        break;
+      }
+      case 5: {
+        break;
+      }
+      default: {
+        printf("Invalid option\n");
+      }
+    }
+  } while (option != 5);
+  // ring = freeRing(ring);
+  return 0;
+}
+
+void menu() {
+  printf("1. Insert a node\n");
+  printf("2. Delete a node\n");
+  printf("3. Print ring\n");
+  printf("4. Free ring\n");
+  printf("5. Exit\n");
+}
+
+void printRing(node *ring) {
+  if (!ring) {
+    printf("Empty.\n");
+    return;
+  }
+  printf("HEAD->");
+  node *t = ring;
+  printf("%d->", t->key);
+  t = t->next;
+  while (t != ring) {
+    printf("%d->", t->key);
+    t = t->next;
+  }
+  printf("HEAD\n");
+}
+
+node *insert(node *ring, int val) {
+  node *q = malloc(sizeof(node));
+  q->key = val;
+  if (!ring) {
+    q->next = q;
+    return q;
+  }
+  node *curr = ring;
+  while (curr->next != ring && !(curr->key <= val && val < curr->next->key)) {
+    curr = curr->next;
+  }
+  q->next = curr->next;
+  curr->next = q;
+  if (val < ring->key) {
+    return q;
+  } else {
+    return ring;
+  }
+}
+
+node *delete(node *ring, int val) {
+  if (!ring) {
+    printf("Empty.\n");
+    return ring;
+  }
+  if (val == ring->key) {
+    node *t = ring;
+    if (ring->next == ring) {
+      free(t);
+      return NULL;
+    } else {
+      node *end = ring;
+      while (end->next != ring) {
+        end = end->next;
+      }
+      ring = t->next;
+      end->next = ring;
+      free(t);
+      return ring;
+    }
+  }
+  node *p = ring;
+  while (p->next != ring) {
+    if (val == p->next->key) {
+      node *t = p->next;
+      p->next = t->next;
+      free(t);
+    } else if (val > p->next->key) {
+      p = p->next;
+    } else {
+      break;
+    }
+  }
+  return ring;
+}
+
+node *freeRing(node *ring) {
+  if (!ring) {
+    return ring;
+  }
+  if (ring->next == ring) {
+    free(ring);
+    return NULL;
+  }
+  node *p = ring->next;
+  while (p != ring) {
+    node *t = p;
+    p = p->next;
+    free(t);
+  }
+  free(p);
+  return NULL;
+}
