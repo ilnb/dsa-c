3e73e8c8f7f5df1c50ef027c1ce8f91c76655d41
lnb <lnbsa12345@gmail.com>
2025-03-04 20:59:24 +0530

    done

---
+++ list/selSort.c
@@ -0,0 +1,42 @@
+#include "node.h"
+
+node *selSort(node *);
+
+int main(void) {
+  node *head = NULL;
+  head = insertStart(head, 2);
+  head = insertStart(head, 1);
+  head = insertStart(head, 5);
+  head = insertStart(head, 3);
+  head = insertStart(head, 4);
+  printf("List right now: ");
+  displayList(head);
+  head = selSort(head);
+  printf("Sorted list: ");
+  displayList(head);
+  head = freeList(head);
+  return 0;
+}
+
+node *selSort(node *head) {
+  if (!head) {
+    return head;
+  }
+  node *p = head;
+  while (p) {
+    node *q, *t;
+    q = p->next;
+    t = p;
+    while (q) {
+      if (q->key < t->key) {
+        t = q;
+      }
+      q = q->next;
+    }
+    int c = t->key;
+    t->key = p->key;
+    p->key = c;
+    p = p->next;
+  }
+  return head;
+}
