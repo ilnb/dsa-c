a530ed35b1cb9c3923e4816f177a06db7967e5f7
lnb <lnbsa12345@gmail.com>
2025-04-25 23:18:23 +0530

    insertion sort?

---
+++ list/inSort.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+  int key;
+  struct node *next;
+} node;
+
+node *inSort(node *);
+node *sortedInsert(node *, int);
+void printList(node *);
+
+int main() {
+  node *head = NULL;
+  for (int i = 0; i < 11; i++)
+    head = sortedInsert(head, i);
+  printList(head);
+  head = inSort(head);
+  printList(head);
+  return 0;
+}
+
+node *inSort(node *head) {
+  if (!head)
+    return head;
+  node *sorted = NULL;
+  node *curr = head;
+  while (curr) {
+    node *next = curr->next;
+    if (!sorted || curr->key < sorted->key) {
+      curr->next = sorted;
+      sorted = curr;
+    } else {
+      node *t = sorted;
+      while (t->next && t->next->key < curr->key)
+        t = t->next;
+      curr->next = t->next;
+      t->next = curr;
+    }
+    curr = next;
+  }
+  return sorted;
+}
+
+node *sortedInsert(node *head, int val) {
+  node *p = (node *)malloc(sizeof(node));
+  if (!p) {
+    printf("Out of memory.\n");
+    return head;
+  }
+  p->key = val;
+  if (!head || val < head->key) {
+    p->next = head;
+    return p;
+  }
+  node *q = head;
+  while (q->next && val >= q->next->key)
+    q = q->next;
+  p->next = q->next;
+  q->next = p;
+  return head;
+}
+
+void printList(node *head) {
+  while (head) {
+    printf("%d->", head->key);
+    head = head->next;
+  }
+  printf("NULL\n");
+}
