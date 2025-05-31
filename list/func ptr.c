0e228d8eedeca3f4c4e2fcf006bad23d0e678af3
lnb <lnbsa12345@gmail.com>
2025-06-01 00:14:36 +0530

    minor updates

--- list/func ptr.c
+++ list/func ptr.c
@@ -2,26 +2,26 @@
 
 typedef node *(*FUNC)(node *, int);
 
-// returns insert / delete function
+// returns insert (ins) / delete (del) function
 FUNC choice(char *choice);
 // node *(*choice(char *choice))(node *, int);
 
 int main() {
   node *head = NULL;
   for (int i = 0; i < 11; i++)
-    head = (*choice("insert"))(head, i);
+    head = (*choice("ins"))(head, i);
   displayList(head);
   for (int i = 0; i < 11; i += 2)
-    head = (*choice("delete"))(head, i);
+    head = (*choice("del"))(head, i);
   displayList(head);
   head = freeList(head);
   return 0;
 }
 
 FUNC choice(char *str) {
-  if (!strcmp(str, "insert"))
+  if (!strcmp(str, "ins"))
     return sortedInsert;
-  else if (!strcmp(str, "delete"))
+  else if (!strcmp(str, "del"))
     return sortedDelete;
   return NULL;
 }
